d182a0b3b98b2f472e1cd477d4d7841fc5cce590
akhelper63 <akhelper63@gmail.com>
2020-02-24 11:52:50 +0330

    create login and register page

--- src/Pages/Login/LoginDashboard.js
+++ src/Pages/Login/LoginDashboard.js
@@ -1,13 +1,79 @@
 import React, { Component } from 'react';
+import validator from 'validator';
+import Toast from 'react-bootstrap/Toast';
+
+import { instance } from '../components/Axios';
+import Footer from '../components/Footer';
+import { connect } from 'react-redux';
+import Menu from '../components/Menu';
+
 
 class LoginDashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mobile: '',
+            password: '',
+            error: null,
+            errordec: '',
+            redir: null,
+            showToast: false
+        }
+    }
     render() {
+
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+
+                <Menu />
+                <section className="mt-5 mb-5">
+                    <div className="container mb-5">
+                        <div className='col-lg-4 offset-lg-4' >
+                            <h2 className="text-center">فرم ورود</h2>
+                            {/* <span className="invalid-feedback rtl" style={{ display: this.state.errordec ? 'block' : 'none' }}>{this.state.errordec}</span> */}
+                            <Toast className="bg-warning" onClose={() => { this.setState({ showToast: false }) }} show={this.state.showToast} delay={3000} autohide >
+                                <Toast.Header>
+                                    <strong className="ml-auto">هشدار</strong>
+                                </Toast.Header>
+                                <Toast.Body>{this.state.errordec}</Toast.Body>
+                            </Toast>
+                            <form onSubmit={this.handleSubmit} style={{ marginTop: 20 }}>
+                                <div className="text-right">
+
+                                    <div className="form-group">
+                                        <label className="text-right">شماره همراه</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="mobile"
+                                            value={this.state.mobile}
+                                            placeholder="لطفا شماره همراه خود را وارد نمایید" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>رمز عبور</label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            name="password"
+                                            value={this.state.password}
+                                            placeholder="لطفا رمز عبور خود را وارد نمایید" />
+                                    </div>
+                                </div>
+                                <div className="form-group text-center">
+                                    <button className="btn btn-danger mt-5" type="submit">ورود</button>
+                                </div>
+
+                            </form>
+
+                        </div>
+                    </div>
+                </section>
+
+                <Footer />
+            </React.Fragment>
         );
     }
 }
+
 
 export default LoginDashboard;
--- src/Pages/Register/RegisterDashboard.js
+++ src/Pages/Register/RegisterDashboard.js
@@ -1,13 +1,85 @@
 import React, { Component } from 'react';
+import { instance } from '../Axios';
+import Toast from 'react-bootstrap/Toast';
 
 class RegisterDashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mobile: "",
+            password: "",
+            repassword: "",
+            rolid: [2],
+            token: "123456789qwertyuiopqaz",
+            error: null,
+            errordec: null,
+            redir: null,
+            showToast: false
+        }
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+                <div className="container mb-5" >
+                    <div className='col-lg-4 offset-lg-4' >
+                        <form className="form-signin" onSubmit={this.handleSubmit} style={{ marginTop: 20 }}>
+                            <h1 className="h3 mb-3 font-weight-normal text-center">ثبت نام پنل آرامیار</h1>
+                            <Toast className="bg-warning" onClose={() => { this.setState({ showToast: false }) }} show={this.state.showToast} delay={3000} autohide >
+                                <Toast.Header>
+                                    <strong className="ml-auto">هشدار</strong>
+                                </Toast.Header>
+                                <Toast.Body>{this.state.errordec}</Toast.Body>
+                            </Toast>
+                            <div className="text-right">
+                                <div className="form-group">
+                                    <label className="">شماره همراه</label>
+                                    <input
+                                        type="text"
+                                        id="mobile"
+                                        name="mobile"
+                                        value={this.state.mobile}
+                                        className="form-control"
+                                        placeholder="شماره همراه"
+                                        required=""
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label className="">رمز عبور</label>
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        name="password"
+                                        value={this.state.password}
+                                        className="form-control"
+                                        placeholder="رمزعبور"
+                                        required=""
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label className="">تکرار رمز عبور</label>
+                                    <input
+                                        type="password"
+                                        id="repassword"
+                                        name="repassword"
+                                        value={this.state.repassword}
+                                        className="form-control"
+                                        placeholder="تکرار رمز عبور"
+                                        required=""
+                                    />
+
+                                </div>
+                            </div>
+                            <div className="form-group text-center">
+                                <button className="btn btn-primary mt-3 " type="submit">ثبت نام</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </ React.Fragment >
         );
     }
 }
+
 
 export default RegisterDashboard;
